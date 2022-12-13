1a9ecb0f33fea0331e533c7b8bc200d0590db960
Qingyu.Meng <11192777@qq.com>
2022-12-13 11:08:17 +0800

    feat: 🎸 add html tag

---
+++ src/main/java/pers/qingyu/tookoff/chore/HtmlTag.java
@@ -0,0 +1,89 @@
+package pers.qingyu.tookoff.chore;
+
+/**
+ * <H1></H1>
+ *
+ * @author Qingyu.Meng
+ * @version 1.0
+ * @date 2022/12/13 09:36
+ */
+public class HtmlTag {
+
+    /**
+     * <a href="https://github.com">github</a> <br>
+     * <b>这个加粗</b> <br>
+     * <big>更大的字体</big> <br>
+     * <blockquote>This is a long quotation. This is a long quotation. This is a long quotation. This is a long quotation. This is a long quotation.</blockquote> <br>
+     * <center>居中</center>
+     *
+     * <cite>what is cite? 参考文献</cite>
+     *
+     * <br><stong>!!!!!</stong>
+     *
+     * <code>for i, i< 10 i ++ ?? </code>
+     *
+     * <h2>一个定义列表</h2>
+     * <dl>
+     *     <dt>标题</dt>
+     *     <dd>1.hhhhh</dd>
+     *     <dd>2.xixixixi</dd>
+     *     <dt>2Biao</dt>
+     *     <dd>skip</dd>
+     * </dl>
+     *
+     * <dfn>what is this？</dfn>
+     *
+     * <div style="color:#00FF00">not user this
+     * <em>what is this</em>
+     * </div>
+     *
+     * <font size="16" color="red">This is font tab.</font>
+     *
+     * <h1>1</h1>
+     * <h2>2</h2> <hr/>
+     * <h3>3</h3>
+     *
+     * <i>清切</i>
+     *
+     * <ol>
+     *    <li>Coffee</li>
+     *    <li>Tea</li>
+     *    <li>Milk</li>
+     * </ol>
+     *
+     * <ul>
+     *    <li>Coffee</li>
+     *    <li>Tea</li>
+     *    <li>Milk</li>
+     * </ul>
+     *
+     * <small>xiao</small>
+     *
+     *
+     * <table border="1">
+     *   <tr>
+     *     <th>Month</th>
+     *     <th>Savings</th>
+     *   </tr>
+     *   <tr>
+     *     <td>January</td>
+     *     <td>$100</td>
+     *   </tr>
+     * </table>
+     *
+     * <table border="1">
+     *   <tr>
+     *     <th>Company</th>
+     *     <th>Address</th>
+     *   </tr>
+     *
+     *   <tr>
+     *     <td>Apple, Inc.</td>
+     *     <td>1 Infinite Loop Cupertino, CA 95014</td>
+     *   </tr>
+     * </table>
+     *
+     * <u>xiahuaxian</u>
+     */
+    private String text;
+}
