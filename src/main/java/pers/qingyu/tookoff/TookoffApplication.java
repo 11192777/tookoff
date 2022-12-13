be41377343501c55f9b933dd86c7ae765fc862fe
Qingyu.Meng <11192777@qq.com>
2022-12-13 09:23:04 +0800

    feat: 🎸 first commit

---
+++ src/main/java/pers/qingyu/tookoff/TookoffApplication.java
@@ -0,0 +1,13 @@
+package pers.qingyu.tookoff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TookoffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TookoffApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pers/qingyu/tookoff/TookoffApplicationTests.java
@@ -0,0 +1,13 @@
+package pers.qingyu.tookoff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TookoffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
